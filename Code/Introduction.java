38f771cadfbe7dffb50d1f067984dc0f32c5d447
Lydia <Lydia.Fritz@gcu.edu>
2018-07-13 12:42:01 -0700

    Exercises
    Exercise 1 file

---
+++ Code/Introduction.java
@@ -0,0 +1,16 @@
+/**
+ * Exercise 1
+ * CST105
+ * @author Lydia.Fritz
+ *
+ */
+
+public class Introduction {
+
+	public static void main(String[] args) {
+		
+		System.out.println("My name is Lydia Fritz.");
+		System.out.println("My favorite color is red.");
+		System.out.println("If I won the lottery, I would sail around the world.");
+	}
+}
